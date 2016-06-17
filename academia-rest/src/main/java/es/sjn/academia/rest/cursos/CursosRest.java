f49a9935262519b15f7283e992614325e897953f
Sergio <sgcharameli@gmail.com>
2016-06-17 20:49:07 +0200

    Primer trasteo con Spring MVC

--- academia-rest/src/main/java/es/sjn/academia/rest/cursos/CursosRest.java
+++ academia-rest/src/main/java/es/sjn/academia/rest/cursos/CursosRest.java
@@ -1,6 +1,8 @@
 package es.sjn.academia.rest.cursos;
 
 import es.sjn.academia.vo.Curso;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,17 +10,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 @RestController
-@RequestMapping("cursos")
+@RequestMapping("/cursos")
 public class CursosRest {
 
+    private static final Logger LOG = LoggerFactory.getLogger(CursosRest.class);
 
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<Curso>> obtenerTodosLosCursos() {
-
+        LOG.info("Obteniendo todos los cursos");
         final List<Curso> cursos = new ArrayList();
         cursos.add(new Curso("nombre", "desc"));
         return new ResponseEntity<List<Curso>>(cursos, HttpStatus.OK);
--- academia-servicio/src/main/java/es/sjn/academia/vo/Curso.java
+++ academia-servicio/src/main/java/es/sjn/academia/vo/Curso.java
@@ -4,7 +4,6 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
 import java.io.Serializable;
-import java.util.HashMap;
 
 public class Curso implements Serializable {
 
